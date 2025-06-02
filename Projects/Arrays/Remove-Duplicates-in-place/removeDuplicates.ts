8104f6e244b4869f90fe006b9ad972f7714846cd
RohanS2022 <rohans2022github@gmail.com>
2025-06-02 00:49:35 -0400

    install jest, add git ignore, remove Dups function

---
+++ Projects/Arrays/Remove-Duplicates-in-place/removeDuplicates.ts
@@ -0,0 +1,9 @@
+/**
+ * @function - Removes duplicates in place so that each unique element appears only once.
+ * @param {number[]} - An array of sorted numbers from smallest to largest.
+ * @return {number[]} - An array of numbers with no duplicates 
+ */
+
+const removeDuplicates = (nums) => {
+    return null
+}
